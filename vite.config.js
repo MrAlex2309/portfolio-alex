7010eb8a66a3386dba55580c630b31f578abb9d4
MrAlex2309 <songheakchanratanak@gmail.com>
2025-06-12 09:56:12 +0700

    Next

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-alex/',
   plugins: [
        react(),
        VitePWA({
@@ -43,5 +44,5 @@ export default defineConfig({
          },
        }),
      ],
-     base: '/portfolio-alex/'
+     
 })
